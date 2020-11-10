3f444f362608e873b026512d63d86f912a39a872
Harry Askham <harryaskham@gmail.com>
2020-11-10 19:43:05 +0000

    working-ide

--- app/Main.hs
+++ app/Main.hs
@@ -2,11 +2,10 @@
 
 module Main where
 
-import Lib ( someFunc )
 import qualified Data.Text as T
 import Data.Text (Text(..))
 
-clientId :: T.Text
+clientId :: Text
 clientId = "28342f3712b74711af0f925204a8aae1"
 
 clientSecret :: IO Text
