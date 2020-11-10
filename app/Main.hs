239aa7ccf2dc5290e94aaff2867e69009bb66a02
Harry Askham <harryaskham@gmail.com>
2020-11-10 19:40:23 +0000

    Purge secret

--- app/Main.hs
+++ app/Main.hs
@@ -9,8 +9,12 @@ import Data.Text (Text(..))
 clientId :: T.Text
 clientId = "28342f3712b74711af0f925204a8aae1"
 
-clientSecret :: Text
-clientSecret = "fa95fe45a19d4105b3fe16ae8c95561a"
+clientSecret :: IO Text
+clientSecret = do
+  content <- readFile "secret"
+  return $ T.pack $ head (lines content)
 
 main :: IO ()
-main = someFunc
+main = do
+  secret <- clientSecret
+  print secret
