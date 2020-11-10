b908f8ad30f5ce3f25f2375d19b9ee7204794b1b
Harry Askham <harryaskham@gmail.com>
2020-11-10 15:51:48 +0000

    Init

---
+++ ChangeLog.md
@@ -0,0 +1,3 @@
+# Changelog for featchains
+
+## Unreleased changes
---
+++ app/Main.hs
@@ -0,0 +1,14 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Lib
+
+clientId :: Text
+clientId = "28342f3712b74711af0f925204a8aae1"
+
+clientSecret :: Text
+clientSecret = "fa95fe45a19d4105b3fe16ae8c95561a"
+
+main :: IO ()
+main = someFunc
