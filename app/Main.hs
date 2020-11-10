a7388623ebc99240a7ad19aa6b91dcb54b7cf3d7
Harry Askham <harryaskham@gmail.com>
2020-11-10 20:55:36 +0000

    addstodos

--- app/Main.hs
+++ app/Main.hs
@@ -5,6 +5,20 @@ module Main where
 import qualified Data.Text as T
 import Data.Text (Text(..))
 
+{- TODO
+Example API call
+Fetch song info
+Figure out collaborators representation
+Artist is name + ID
+Write BFS graph scraper
+Define data model
+Scrape + persist graph
+Look at graphDB options
+Build graph inmem
+On the fly bfs / dfs
+All-pairs caching?
+-}
+
 clientId :: Text
 clientId = "28342f3712b74711af0f925204a8aae1"
 
